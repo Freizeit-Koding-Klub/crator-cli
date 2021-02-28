65633928949ac89d700deadfbcff0d938b41b08b
Daniel Wolf <daniel.wolf@8mylez.com>
2021-02-28 11:56:37 +0100

    Fix version helper.

--- src/Services/VersionHelper.php
+++ src/Services/VersionHelper.php
@@ -5,8 +5,6 @@ namespace App\Services;
 class VersionHelper {
     public static function getCurrentVersion(): string
     {
-        return '1.0.0';
-
         //this will be automatically replaced during box compile
         return '@GITVERSION@';
     }
